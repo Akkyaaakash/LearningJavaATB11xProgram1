76dd1d86535c439dfbdc32ea353b3d22b666f84c
Akash <akashmaurya1996v@gmail.com>
2025-03-18 07:28:17 +0530

    Increment Decrement Operator

---
+++ src/ex_07_increment_decrement_op/lab_003_ID.java
@@ -0,0 +1,10 @@
+package ex_07_increment_decrement_op;
+
+public class lab_003_ID {
+    public static void main(String[] args) {
+        int a =20;
+
+        System.out.println(--a + a++ + a--);
+        System.out.println(a);
+    }
+}
